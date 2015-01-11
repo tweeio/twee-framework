380b3b834bb3a6b9d00bced7cd45b00a0c2bbe39
Dmitri Mesin <dmitriym@elance-odesk.com>
2015-01-11 10:32:36 +0200

    Grunt works with watch. Restart server on server-side files change, recompile assets on client-side files changing. Collecting grunt tasks from modules

--- templates/application/Gruntfile.js
+++ templates/application/Gruntfile.js
@@ -2,68 +2,14 @@ module.exports = function(grunt) {
     require('load-grunt-tasks')(grunt);
 
     // Running notify
-    //grunt.task.run('notify_hooks');
+    grunt.task.run('notify_hooks');
 
     //build scripts
-    grunt.registerTask('default', ['bower', 'concat', 'uglify', 'cssmin', 'copy']);
+    grunt.registerTask('default', ['bower', 'build', 'watch']);
+    grunt.registerTask('build', ['concat', 'uglify', 'cssmin', 'copy']);
 
     var defaultConfig = {
-        pkg: grunt.file.readJSON('package.json'),
-        watch: {
-            twee: {
-                files:  [
-                    '**/*.*',
-                    '!**node_modules**',
-                    '!**.git**',
-                    '!**.idea**'
-                ],
-                tasks:  ['build-dev', 'express:dev', 'notify:server'],
-                options: {
-                    // For grunt-contrib-watch v0.5.0+, "nospawn: true" for lower versions.
-                    // Without this option specified express won't be reloaded
-                    spawn: false
-                }
-            }
-        },
-        twee: {
-            options: {
-                script: './application.js'
-            },
-            dev: {
-                options: {
-                    port: 3000,
-                    node_env: 'development',
-                    background: false
-                }
-            },
-            prod: {
-                options: {
-                    node_env: 'production',
-                    port: 80,
-                    background: false
-                }
-            }
-        },
-        notify: {
-            js: {
-                options: {
-                    title: 'Twee Grunt',
-                    message: 'JavaScript Building Done'
-                }
-            },
-            css: {
-                options: {
-                    title: 'Twee Grunt',
-                    message: 'Styles Building Done'
-                }
-            },
-            server: {
-                options: {
-                    title: 'Twee Grunt',
-                    message: 'Server is ready!'
-                }
-            }
-        }
+        pkg: grunt.file.readJSON('package.json')
     };
 
     var twee = require('twee').setBaseDirectory(__dirname);
--- templates/application/modules/_Twee-MNT_/setup/configs/grunt.js
+++ templates/application/modules/_Twee-MNT_/setup/configs/grunt.js
@@ -1,16 +1,59 @@
 module.exports = {
     "concat": {
-        "default-js": {
+        "_Twee-MNT-LC_-js": {
             "src": [
-                "foo/bar.js"
+                "modules/_Twee-MNT-LC_/assets/js/*.js",
+                "modules/_Twee-MNT-LC_/assets/js/**/*.js"
             ],
-            "dest": "public/build/<%= pkg.version %>/default.<%= pkg.version %>.js"
+            "dest": "public/build/<%= pkg.version %>/js/_Twee-MNT-LC_.<%= pkg.version %>.js"
         },
-        "default-css": {
+        "_Twee-MNT-LC_-css": {
             "src": [
-                "bas/stop.css"
+                "modules/_Twee-MNT-LC_/assets/css/*.css",
+                "modules/_Twee-MNT-LC_/assets/css/**/*.css"
             ],
-            "dest": "public/build/<%= pkg.version %>/default.<%= pkg.version %>.css"
+            "dest": "public/build/<%= pkg.version %>/css/_Twee-MNT-LC_.<%= pkg.version %>.css"
+        }
+    },
+    uglify: {
+        "_Twee-MNT-LC_": {
+            files: {
+                'public/build/<%= pkg.version %>/js/_Twee-MNT-LC_.<%= pkg.version %>.min.js': ['public/build/<%= pkg.version %>/js/_Twee-MNT-LC_.<%= pkg.version %>.js']
+            }
+        },
+        options: {
+            compress: true
+        }
+    },
+    cssmin: {
+        "_Twee-MNT-LC_": {
+            files: {
+                'public/build/<%= pkg.version %>/css/_Twee-MNT-LC_.<%= pkg.version %>.min.css': ['public/build/<%= pkg.version %>/css/_Twee-MNT-LC_.<%= pkg.version %>.css']
+            }
+        }
+    },
+    copy: {
+        "_Twee-MNT-LC_-img": {
+            files: [
+                {
+                    expand: true,
+                    flatten: true,
+                    //filter: 'isFile',
+                    src: ["modules/_Twee-MNT-LC_/assets/img/*.*"],
+                    dest: 'public/build/<%= pkg.version %>/img/_Twee-MNT-LC_'
+                }
+            ]
+        },
+        "_Twee-MNT-LC_-fonts": {
+            files: [
+                {
+                    expand: true,
+                    flatten: true,
+                    //filter: 'isFile',
+                    src: ["modules/_Twee-MNT-LC_/assets/fonts/*.*"],
+                    dest: 'public/build/<%= pkg.version %>/fonts/_Twee-MNT-LC_'
+                }
+            ]
         }
     }
 };
