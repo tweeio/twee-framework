1d3f59b5c7b29580dab1759aea28c0908e3a7cf9
Dmitri Mesin <dmitriym@elance-odesk.com>
2015-03-11 23:30:59 +0200

    New kernel

--- configs/default.js
+++ configs/default.js
@@ -1,38 +1,4 @@
 module.exports = {
-    "extensions": {
-        "HTTP Parser": {
-            "module": "twee-http-parser-extension"
-        },
-
-        "Winston Logger": {
-            "module": "twee-logging-extension"
-        },
-
-        "`Powered With` Header": {
-            "module": "twee-powered-extension"
-        },
-
-        "Static Files": {
-            "module": "twee-static-extension"
-        },
-
-        "HTML Compressor": {
-            "module": "twee-compressor-extension"
-        },
-
-        "View Engines": {
-            "module": "twee-view-extension"
-        },
-
-        "View Helpers": {
-            "module": "twee-view-extension/helpers"
-        },
-
-        "i18n": {
-            "module": "twee-i18n-extension"
-        }
-    },
-
     "options": {
         "errorPages": {
             "404": {
